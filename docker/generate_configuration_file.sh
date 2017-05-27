26b246d2e59162123fe7adedcc1b13d61a19ea34
Evgeni Pandurski <epandurski@gmail.com>
2017-05-27 21:21:03 +0300

    copy the ssl cert from /run/secrets if available

--- docker/generate_configuration_file.sh
+++ docker/generate_configuration_file.sh
@@ -4,4 +4,8 @@
 
 set -e
 envsubst '$PROXY_PASS_TO:$CMBARTER_HOST' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+if [[ -e /run/secrets/cert.pem ]] && [[ -e /run/secrets/key.pem ]]; then
+   cp /run/secrets/cert.pem /run/secrets/key.pem /etc/nginx/ssl/
+fi
+
 exec "$@"
