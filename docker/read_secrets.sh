16165d046133f9121b6fa8ecdf03a7b035fb148c
Evgeni Pandurski <epandurski@gmail.com>
2017-06-07 22:12:25 +0300

    use pgbouncer in Dockerfile-web

--- docker/read_secrets.sh
+++ docker/read_secrets.sh
@@ -17,4 +17,34 @@ if [[ -z $CMBARTER_REGISTRATION_SECRET ]]; then
     fi
 fi
 
+mkdir -p /var/log/postgresql
+chmod -R 755 /var/log/postgresql
+chown -R postgres:postgres /var/log/postgresql
+
+mkdir -p /etc/pgbouncer
+touch /etc/pgbouncer/userlist.txt
+cat > /etc/pgbouncer/pgbouncer.ini <<EOF
+[databases]
+cmbarter = ${CMBARTER_DSN}
+
+[pgbouncer]
+listen_addr = 127.0.0.1
+listen_port = 5432
+logfile = /var/log/postgresql/pgbouncer.log
+pidfile = /var/run/postgresql/pgbouncer.pid
+unix_socket_dir = /var/run/postgresql
+auth_type = any
+auth_file = /etc/pgbouncer/userlist.txt
+pool_mode = session
+server_reset_query = DISCARD ALL
+max_client_conn = 100
+default_pool_size = 20
+dns_max_ttl = 1
+EOF
+
+rm -f /var/run/postgresql/pgbouncer.pid
+pgbouncer -q -u postgres /etc/pgbouncer/pgbouncer.ini &
+
+export CMBARTER_DSN="dbname=cmbarter"
+
 exec "$@"
