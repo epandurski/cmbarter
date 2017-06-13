3db310fc57e07b1122fb37cadcaa025fa30d7b7e
Evgeni Pandurski <epandurski@gmail.com>
2017-06-13 17:42:12 +0300

    add iptables rules in proxy

--- docker/configure_nginx.sh
+++ docker/configure_nginx.sh
@@ -8,4 +8,8 @@ if [[ -e /run/secrets/cert.pem ]] && [[ -e /run/secrets/key.pem ]]; then
    cp /run/secrets/cert.pem /run/secrets/key.pem /etc/nginx/ssl/
 fi
 
+iptables -P INPUT DROP
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -m state --state NEW -m limit --limit 5000/second --limit-burst 250 -j ACCEPT
+
 exec "$@"
